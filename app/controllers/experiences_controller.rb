7fed4e0e7e16685f1b0c5acf8ec2819f537e282d
andy tutton <andytutton2@gmail.com>
2024-05-03 21:14:41 +0200

    CRUDing

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -6,4 +6,38 @@ class ExperiencesController < ApplicationController
   def show
     @experience = Experience.find(params[:id])
   end
+
+  def new
+    @experience = Experience.new
+  end
+
+  def create
+    @experience = Experience.new(experience_params)
+
+    if @experience.save
+      redirect_to @experience
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @experience = Experience.find(params[:id])
+  end
+
+  def update
+    @experience = Experience.find(params[:id])
+  end
+
+  def destroy
+    @experience = Experience.find(params[:id])
+    @experience.destroy
+  end
+
+  private
+    def experience_params
+      params.require(:experience).permit(:title, :description, :country, :region, images: [])
+    end
+
+
 end
--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,2 +1,8 @@
 class Experience < ApplicationRecord
+  has_many_attached :images
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :country, presence: true
+  validates :region, presence: true
+  validates :images, presence: true
 end
